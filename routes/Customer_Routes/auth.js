986309c12f0a60f004e91fbe8c3182c2543a72f3
Rakesh Biswal <rb2306114@gmail.com>
2026-01-08 22:49:28 +0530

    nkj

--- routes/Customer_Routes/auth.js
+++ routes/Customer_Routes/auth.js
@@ -183,7 +183,7 @@ router.post('/signin', async (req, res) => {
     res.cookie('customerToken', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
