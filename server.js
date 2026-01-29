bacbdfec30c16320a3059934eedd548636c27051
Rakesh Biswal <rb2306114@gmail.com>
2026-01-29 20:08:37 +0530

    changed

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.use("/api/cms/vendors", vendorRoutes);
 const rawItemsRoutes = require("./routes/CMS_Routes/Inventory/Products/rawItems");
 app.use("/api/cms/raw-items", rawItemsRoutes);
 
-const stockItemsRoutes = require("./routes/CMS_Routes/Inventory/Products/stockItems");
+const stockItemsRoutes = require("./routes/CMS_Routes/Inventory/Products/StockItems");
 app.use("/api/cms/stock-items", stockItemsRoutes);
 
 // Operations Category
