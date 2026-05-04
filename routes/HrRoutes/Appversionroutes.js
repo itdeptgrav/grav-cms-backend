a334e61148200ffed2d54c3c00acf78ef5f8b545
itdeptgrav <itdeptgrav@gmail.com>
2026-05-04 20:05:11 +0530

    Fix import case for AppVersion model

--- routes/HrRoutes/Appversionroutes.js
+++ routes/HrRoutes/Appversionroutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const AppVersion = require("../../models/AppVersion");
+const AppVersion = require("../../models/Appversion");
 const EmployeeAuthMiddleware = require("../../Middlewear/EmployeeAuthMiddlewear");
 const { uploadToGoogleDrive } = require("../../services/mediaUpload.service");
 
