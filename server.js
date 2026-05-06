7428350cdb4a1c9a514c3ab4ed872c3c6cc4b217
itdeptgrav <itdeptgrav@gmail.com>
2026-05-06 09:57:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1827,7 +1827,7 @@ app.get("/", (req, res) => {
 const payslipRoutes = require("./routes/Employee_Routes/Payslip");
 app.use("/api/employee/payslip", payslipRoutes);
 
-const overtimeRoutes = require("./routes/Employee_Routes/OvertimeRoutes");
+const overtimeRoutes = require("./routes/Employee_Routes/Overtimeroutes");
 app.use("/api/employee/overtime", overtimeRoutes);
 overtimeRoutes.startOvertimeReminders(io);
 
